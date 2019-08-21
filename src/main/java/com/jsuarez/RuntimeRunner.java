58d6b466e44e445880e59f918e621c587c98f4ab
TakashiNakanowatari <t.nakanowatari@vinx.co.jp>
2019-08-21 11:05:28 +0900

    talend on heroku
    talend on heroku

---
+++ src/main/java/com/jsuarez/RuntimeRunner.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsuarez;
+
+import local_project.post_test_job_0_1.Post_Test_Job;
+
+/**
+ *
+ * @author jsuarez
+ */
+public class RuntimeRunner {
+    
+    public static void main(String[] args){        
+        Post_Test_Job.main(args);
+    }
+    
+}
